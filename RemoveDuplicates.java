1aa4ab0b81ea2783cdffd34f022909e74762a488
nayan2498 <nayanjoshi74@gmail.com>
2026-09-02 15:45:36 +0530

    first

---
+++ RemoveDuplicates.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 2, 3, 4, 4, 5);
+        List<Integer> unique = numbers.stream().distinct().collect(Collectors.toList());
+        System.out.println(unique); 
+    }
+}
