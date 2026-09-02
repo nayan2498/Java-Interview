78e5d111f50c665c4891ccb3650bb508e299a941
nayan2498 <nayanjoshi74@gmail.com>
2026-09-02 21:07:03 +0530

    two

--- CheckPalindrome.java
+++ CheckPalindrome.java
@@ -1,6 +1,6 @@
 public class CheckPalindrome {
     public static void main(String[] args) {
-        String str = "nayanerererttttttt";
+        String str = "nayan";
         String reverse = "";
 
         for (int i = str.length() - 1; i >= 0; i--) {
---
+++ FirstNonRepeatedCharacter.java
@@ -0,0 +1,24 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FirstNonRepeatedCharacter {
+    public static void main(String[] args) {
+        String str = "swiss";
+        Character result = str.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        LinkedHashMap::new,
+                        Collectors.counting()
+                ))
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() == 1)
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(null);
+        System.out.println(result);        
+    }
+}
